7064be6fb99b4273a39d50fa5d380e5aecc05c7c
stefan-hacks <stefanmrobertson@gmail.com>
2025-08-24 02:49:14 -0600

    updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,7 +30,7 @@ handle_phase() {
         sudo apt update
         sudo apt install -y nala gnome-software-plugin-flatpak git gh stow build-essential dkms \
             linux-headers-$(uname -r) curl wget cmake gawk font-manager gnome-clocks \
-            gnome-weather gnome-shell-extension-manager gpaste-2 fail2ban ufw snapd flatpak \
+            guvcview gnome-weather gnome-shell-extension-manager gpaste-2 fail2ban ufw snapd flatpak \
             gdebi tor torbrowser-launcher ffmpeg yt-dlp vlc mpv kitty figlet lolcat btop \
             gir1.2-gtop-2.0 lm-sensors hx nvim fzf ripgrep fd-find fastfetch cpufetch cmatrix hollywood
 
